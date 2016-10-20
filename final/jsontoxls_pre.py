5507573bc3a3e64802bd70faf75c0c768eb683eb
Sebastin Santy <sebastinssanty@gmail.com>
2016-10-21 02:45:39 +0530

    Tagging feature included

--- final/jsontoxls_pre.py
+++ final/jsontoxls_pre.py
@@ -1,13 +1,75 @@
 import xlwt
 import simplejson as json
 import os
-from logic import branch, psts, getcoursetype, proj
 
 with open(os.path.join('json',"coursedesc.json")) as json_file:
+    print('hello')
     coursedesc_arr = json.load(json_file)
 
-with open(os.path.join('json',"noofcourse.json")) as json_file:
-    noofcourse = json.load(json_file)
+#Get the branch from the Campus ID given
+def branch(s):
+    btype = s[4:8]
+    if (btype[2:4] == 'PS') or (btype[2:4] == 'TS'):
+        return btype[0:2]
+    if (btype[0] == 'A') and (btype[2] == 'B'):
+        return (btype[2:4] + btype[0:2])
+    else:
+        return btype
+
+def psts(s):
+    btype = s[4:8]
+    if (btype[2:4] == 'TS'):
+        return True
+    else:
+        return False
+
+#Check for Reverse Dual/ BE Dual Cases
+def specialcase(s):
+    btype = s[4:8]
+    if btype[2:3] == 'B':
+        return True
+
+
+#Get the course type by comparing it with the branch, like whether it is an elective/CDC
+def getcoursetype(compcode, cid, branch):
+    if (compcode == '21024') or (compcode == '21023'):
+        return 'Other'
+    if (compcode == '21591') and (psts(cid)):
+        return 'OPEN'
+    try:
+        coursedesc_arr[compcode]
+    except:
+        return 'OPEN'
+    branch1 = branch[0:2]
+    tag1 = list(filter(lambda x: branch1 in x, coursedesc_arr[compcode]['Tag']))
+    branch2 = ''
+    tag2 = []
+    if len(branch) == 4:
+        branch2 = branch[2:4]
+        # print(branch2)
+        tag2 = list(filter(lambda x: branch2 in x, coursedesc_arr[compcode]['Tag']))
+        # print(tag2)
+    tag3 = list(filter(lambda x: 'HUM' in x, coursedesc_arr[compcode]['Tag']))
+    # print(tag1)
+    if not tag1 and not tag2 and not tag3:
+        return 'OPEN'
+    elif tag3:
+        return 'HUM'
+    elif tag1 and tag1[0][2:4] == 'EL':
+        return 'DEL1'
+    elif tag2 and tag2[0][2:4] == 'EL':
+        return 'DEL2'
+    elif tag1:
+        if ('CDC' in z for z in tag1[0]):
+            return 'CDC'
+    elif tag2:
+         if ('CDC' in y for y in tag2[0]):
+            return 'CDC'
+    else:
+        return 'OPEN'
+
+
+
 
 sheetno = 1
 
@@ -41,71 +103,24 @@ for i in u:
     sheet.write(c, 6, i['Catalog No'].split())
     sheet.write(c, 7, i['Unit Taken'])
     sheet.write(c, 8, i['Course Grade'])
-    coursecode = i['Subject'] + " " + ''.join(i['Catalog No'].split())
-    coursetype = getcoursetype(coursecode, i['Campus Id'], branch(i['Campus Id']))
-    if (emplid != i['Empl Id']):
-        for j in noofcourse:
-            if (j['Discipline'] == branch(i['Campus Id'])):
-                CDC_REQ = j['No of Courses']['CDC']
-                DEL1_REQ = j['No of Courses']['DEL1']
-                DEL2_REQ = j['No of Courses']['DEL2']
-                HUM_REQ = j['No of Courses']['HUM']
-                OPEN_REQ = j['No of Courses']['OPEN']
-                break
-        CDC_LEFT = CDC_REQ
-        DEL1_LEFT = DEL1_REQ
-        DEL2_LEFT = DEL2_REQ
-        HUM_LEFT = HUM_REQ
-        OPEN_LEFT = OPEN_REQ
-        print(CDC_REQ)
-    FINAL = 'Not parse'
-    emplid = i['Empl Id']
-    if coursetype == 'CDC':
-        CDC_LEFT = CDC_LEFT - 1
-    elif (coursetype == 'HUM' and HUM_LEFT <=0) or (coursetype == 'DEL1' and DEL1_LEFT<=0) or (coursetype == 'DEL2' and DEL2_LEFT<=0 and DEL2_REQ!=0) or (coursetype == 'OPEN'):
-        OPEN_LEFT = OPEN_LEFT - 1
-    elif coursetype == 'DEL1':
-        DEL1_LEFT = DEL1_LEFT - 1
-    elif coursetype == 'DEL2':
-        DEL2_LEFT = DEL2_LEFT - 1
-    elif coursetype == 'HUM':
-        HUM_LEFT = HUM_LEFT - 1
-    try:
-        tag[coursetype].append(coursecode)
-    except:
-        tag[coursetype] = [coursecode]
-    
-    #Extra Flags as mentioned by ARC
-    if proj(coursecode):
-        try:
-            PROJ_LIST[coursecode] = PROJ_LIST[coursecode] + 1
-        except:
-            PROJ_LIST[coursecode] = 1
-    
-    if (ELEC_FLAG==0) and ((coursetype == 'HUM') or (coursetype == 'DEL1') or (coursetype == 'DEL2')) and coursedesc_arr[coursecode]['Units'] < 3:
-        ELEC_FLAG = 1
+    compcode = str(i['Course Id'])
+    print(compcode)
+    coursetype = getcoursetype(compcode, i['Campus Id'], branch(i['Campus Id']))
 
-    if ((coursecode == 'MGTS F211') or (coursecode == 'ECON F211')) and branch(i['Campus Id'])[0:2]!='B3':
-        if POMPOE == 1:
-            OPEN_LEFT = OPEN_LEFT - 1
-        else:
-            POMPOE = 1
-
-    # print(coursecode)
-    sheet.write(c, 9, FINAL)
+    sheet.write(c, 9, coursetype)
     if c>=65535:
         c=0
         print("New sheet")
         sheetno = sheetno + 1
         sheet.col(0).width = 256 * 15
         sheet.col(1).width = 256 * 15
-        sheet.col(2).width = 256 * 35
-        sheet.col(3).width = 256 * 25
+        sheet.col(2).width = 256 * 40
+        sheet.col(3).width = 256 * 5
         sheet.col(4).width = 256 * 6
-        sheet.col(5).width = 256 * 5
-        sheet.col(6).width = 256 * 6
-        sheet.col(7).width = 256 * 3
-        sheet.col(8).width = 256 * 3
+        sheet.col(5).width = 256 * 10
+        sheet.col(6).width = 256 * 7
+        sheet.col(7).width = 256 * 5
+        sheet.col(8).width = 256 * 5
         sheet = workbook.add_sheet("Sheet " + str(sheetno))
         sheet.write(0, 0, 'Empl Id')
         sheet.write(0, 1, 'Campus Id')
@@ -120,12 +135,12 @@ for i in u:
 
 sheet.col(0).width = 256 * 15
 sheet.col(1).width = 256 * 15
-sheet.col(2).width = 256 * 35
-sheet.col(3).width = 256 * 25
+sheet.col(2).width = 256 * 40
+sheet.col(3).width = 256 * 5
 sheet.col(4).width = 256 * 6
-sheet.col(5).width = 256 * 5
-sheet.col(6).width = 256 * 6
-sheet.col(7).width = 256 * 3
-sheet.col(8).width = 256 * 3
+sheet.col(5).width = 256 * 10
+sheet.col(6).width = 256 * 7
+sheet.col(7).width = 256 * 5
+sheet.col(8).width = 256 * 5
 
 workbook.save(os.path.join('result',"final_tag.xls"))
